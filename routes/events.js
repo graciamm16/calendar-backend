7a8b916a340dadbea560e885ec7083856250911a
graciamm16 <graciamartin007@gmail.com>
2025-04-10 11:47:38 +0200

    proyecto actualizado

--- routes/events.js
+++ routes/events.js
@@ -4,23 +4,38 @@
 */
 
 const {Router} = require('express');
+const {check} = require('express-validator');
+const {validarCampos} = require('../middlewares/validar-campos');
 const {validarJWT} = require('../middlewares/validar-jwt');
 const {getEventos, crearEvento, actualizarEvento, eliminarEvento} = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 const router = Router();
 
 
-// Todas tienen que pasar por la validación del JWT.
+// Todas tienen que pasar por la validación del JWT y deben estar protegidas
+router.use(validarJWT);
+
 // Obtener eventos
-router.get('/', validarJWT, getEventos);
+router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', validarJWT, crearEvento);
+router.post(
+    '/',
+    // Validaciones de los campos necesarios
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'La fecha de finalización es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    crearEvento
+);
 
 // Actualizar evento
-router.put('/:id', validarJWT, actualizarEvento);
+router.put('/:id', actualizarEvento);
 
 // Borrar evento
-router.delete('/:id', validarJWT, eliminarEvento);
+router.delete('/:id', eliminarEvento);
 
 
 module.exports = router;
